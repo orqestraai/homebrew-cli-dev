41c133f3b9d94287546555f3004e56a12041bb5a
Ilya Kritsmer <ilyakritsmer@Ilyas-MacBook-Air.local>
2026-05-18 16:51:29 +0300

    fix: point orqestra-dev formula to public latest-dev release

--- Formula/orqestra-dev.rb
+++ Formula/orqestra-dev.rb
@@ -5,7 +5,7 @@ class OrqestraDev < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/orqestraai/orqestra/releases/download/latest-dev/orqestra-macos-arm64"
+      url "https://github.com/orqestraai/orqestra-releases/releases/download/latest-dev/orqestra-macos-arm64"
       sha256 "e1ce9fa5fea479cd911d1165b59a92dc843f13828fba0e4a15934bb0dfc2517d"
     end
   end
